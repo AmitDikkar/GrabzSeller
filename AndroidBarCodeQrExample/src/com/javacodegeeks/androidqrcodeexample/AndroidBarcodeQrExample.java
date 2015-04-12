ef1757f0059a2d8cf62a0c96bd550dd9065d7449
Amit <amit.dikkar@gmail.com>
2015-04-11 21:47:09 -0700

    Initial Commit

---
+++ AndroidBarCodeQrExample/src/com/javacodegeeks/androidqrcodeexample/AndroidBarcodeQrExample.java
@@ -0,0 +1,100 @@
+package com.javacodegeeks.androidqrcodeexample;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+import android.widget.Spinner;
+
+public class AndroidBarcodeQrExample extends Activity {
+	/** Called when the activity is first created. */
+
+	static final String ACTION_SCAN = "com.google.zxing.client.android.SCAN";
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		populateSpinner();
+	}
+
+	private void populateSpinner() {
+		Spinner spinner = (Spinner) findViewById(R.id.spinner_aisleNames);
+		// Create an ArrayAdapter using the string array and a default spinner layout
+		/*ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+		        R.array.planets_array, android.R.layout.simple_spinner_item);*/
+		
+		List<String> aisleNames = new ArrayList<String>();
+		aisleNames.add("amit");
+		aisleNames.add("sjsu");
+		aisleNames.add("Colonnade");
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, aisleNames);
+		// Specify the layout to use when the list of choices appears
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		// Apply the adapter to the spinner
+		spinner.setAdapter(adapter);
+	}
+
+	public void scanBar(View v) {
+		try {
+			Intent intent = new Intent(ACTION_SCAN);
+			intent.putExtra("SCAN_MODE", "PRODUCT_MODE");
+			startActivityForResult(intent, 0);
+		} catch (ActivityNotFoundException anfe) {
+			showDialog(AndroidBarcodeQrExample.this, "No Scanner Found", "Download a scanner code activity?", "Yes", "No").show();
+		}
+	}
+
+	public void scanQR(View v) {
+		try {
+			Intent intent = new Intent(ACTION_SCAN);
+			intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+			startActivityForResult(intent, 0);
+		} catch (ActivityNotFoundException anfe) {
+			showDialog(AndroidBarcodeQrExample.this, "No Scanner Found", "Download a scanner code activity?", "Yes", "No").show();
+		}
+	}
+
+	private static AlertDialog showDialog(final Activity act, CharSequence title, CharSequence message, CharSequence buttonYes, CharSequence buttonNo) {
+		AlertDialog.Builder downloadDialog = new AlertDialog.Builder(act);
+		downloadDialog.setTitle(title);
+		downloadDialog.setMessage(message);
+		downloadDialog.setPositiveButton(buttonYes, new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialogInterface, int i) {
+				Uri uri = Uri.parse("market://search?q=pname:" + "com.google.zxing.client.android");
+				Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+				try {
+					act.startActivity(intent);
+				} catch (ActivityNotFoundException anfe) {
+
+				}
+			}
+		});
+		downloadDialog.setNegativeButton(buttonNo, new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialogInterface, int i) {
+			}
+		});
+		return downloadDialog.show();
+	}
+
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		if (requestCode == 0) {
+			if (resultCode == RESULT_OK) {
+				String contents = intent.getStringExtra("SCAN_RESULT");
+				String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+
+				Toast toast = Toast.makeText(this, "Content:" + contents + " Format:" + format, Toast.LENGTH_LONG);
+				toast.show();
+			}
+		}
+	}
+}
